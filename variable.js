42b87eaf79eaf4a8ff9b0a90be931f977289fd5e
mitrang <mitrang.lad@smartsensesolutions.com>
2022-05-16 14:26:01 +0530

    add variable playground area

---
+++ variable.js
@@ -0,0 +1,25 @@
+// let person = {
+//     name: 'John',
+//     age: 21
+// }
+
+// function updatePerson(obj) {
+//     obj = {
+//         name: 'Sparrow',
+//         age: 22
+//     };
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var arr = [15, 37];
+
+function change(arr) {
+    arr.push(24);
+    debugger;
+    // arrA = [1,2,3]
+}
+
+change(arr);
+console.log(arr);
